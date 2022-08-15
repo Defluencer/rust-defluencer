16a1ab0d67268b24fe91a26de6e65b258e889ffd
SionoiS <SionoiS@users.noreply.github.com>
2022-08-15 11:23:27 -0400

    Chat Message Tweak

--- linked-data/src/media/chat.rs
+++ linked-data/src/media/chat.rs
@@ -1,19 +1,17 @@
 use crate::moderation::{Ban, Moderator};
 
-use crate::types::IPLDLink;
+use crate::types::{IPLDLink, PeerId};
 
 use serde::{Deserialize, Serialize};
 
 /// GossipSub Live Chat Message.
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
 pub struct ChatMessage {
-    /// Name of the sender
-    pub name: String,
-
     /// Usualy text, ban user or add moderator.
     pub message: MessageType,
 
     /// Link to DAG-JOSE block for verification.
+    /// This block links to ChatInfo
     pub signature: IPLDLink,
 }
 
@@ -23,4 +21,13 @@ pub enum MessageType {
     Text(String),
     Ban(Ban),
     Mod(Moderator),
+}
+
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+pub struct ChatInfo {
+    /// Name of the sender
+    pub name: String,
+
+    /// Node used to chat
+    pub node: PeerId,
 }
