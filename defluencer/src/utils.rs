c404634ce8f1dbe95d9d7927f05f31cc40126c74
SionoiS <SionoiS@users.noreply.github.com>
2022-09-20 11:20:35 -0400

    Remove Mimetyped inline data

--- defluencer/src/utils.rs
+++ defluencer/src/utils.rs
@@ -2,13 +2,9 @@ use crate::errors::Error;
 
 use chrono::{DateTime, Datelike, Timelike, Utc};
 
-use cid::multibase::Base;
-
 use ipfs_api::{responses::Codec, IpfsService};
 
 use linked_data::media::mime_type::MimeTyped;
-
-use either::Either;
 
 use cid::Cid;
 
@@ -24,7 +20,7 @@ pub async fn add_image(ipfs: &IpfsService, file: web_sys::File) -> Result<Cid, E
         return Err(Error::Image);
     };
 
-    let size = file.size() as usize;
+    //let size = file.size() as usize;
 
     let js_value = match wasm_bindgen_futures::JsFuture::from(file.array_buffer()).await {
         Ok(js_value) => js_value,
@@ -38,19 +34,13 @@ pub async fn add_image(ipfs: &IpfsService, file: web_sys::File) -> Result<Cid, E
     let uint8_array: Uint8Array = Uint8Array::new(&array_buffer);
     let vec = uint8_array.to_vec();
 
-    let mime_typed = if size > 1_000_000 {
-        let bytes = Bytes::from(vec);
-        let cid = ipfs.add(bytes).await?;
+    let bytes = Bytes::from(vec);
 
-        MimeTyped {
-            mime_type,
-            data: either::Either::Left(cid.into()),
-        }
-    } else {
-        MimeTyped {
-            mime_type,
-            data: either::Either::Right(vec),
-        }
+    let cid = ipfs.add(bytes).await?;
+
+    let mime_typed = MimeTyped {
+        mime_type,
+        data: cid.into(),
     };
 
     let cid = ipfs.dag_put(&mime_typed, Codec::default()).await?;
@@ -60,7 +50,7 @@ pub async fn add_image(ipfs: &IpfsService, file: web_sys::File) -> Result<Cid, E
 
 #[cfg(not(target_arch = "wasm32"))]
 pub async fn add_image(ipfs: &IpfsService, path: &std::path::Path) -> Result<Cid, Error> {
-    use tokio::{fs::File, io::AsyncReadExt};
+    use tokio::fs::File;
 
     let mime_type = match mime_guess::MimeGuess::from_path(path).first_raw() {
         Some(mime) => mime.to_owned(),
@@ -71,25 +61,15 @@ pub async fn add_image(ipfs: &IpfsService, path: &std::path::Path) -> Result<Cid
         return Err(Error::Image);
     };
 
-    let mut file = File::open(path).await?;
-    let meta = file.metadata().await?;
+    let file = File::open(path).await?;
 
-    let mime_typed = if meta.len() > 1_000_000 {
-        let stream = tokio_util::io::ReaderStream::new(file);
-        let cid = ipfs.add(stream).await?;
+    let stream = tokio_util::io::ReaderStream::new(file);
 
-        MimeTyped {
-            mime_type,
-            data: either::Either::Left(cid.into()),
-        }
-    } else {
-        let mut buffer = Vec::new();
-        file.read_to_end(&mut buffer).await?;
+    let cid = ipfs.add(stream).await?;
 
-        MimeTyped {
-            mime_type,
-            data: either::Either::Right(buffer),
-        }
+    let mime_typed = MimeTyped {
+        mime_type,
+        data: cid.into(),
     };
 
     let cid = ipfs.dag_put(&mime_typed, Codec::default()).await?;
@@ -147,26 +127,6 @@ pub async fn add_markdown(ipfs: &IpfsService, file: web_sys::File) -> Result<Cid
     let cid = ipfs.add(bytes).await?;
 
     Ok(cid)
-}
-
-pub async fn data_url(ipfs: &IpfsService, mime_type: &MimeTyped) -> Result<String, Error> {
-    let mut data_url = String::from("data:");
-
-    data_url.push_str(&mime_type.mime_type);
-    data_url.push_str(";base64,");
-
-    let data = match &mime_type.data {
-        Either::Right(data) => Base::Base64.encode(data),
-        Either::Left(cid) => {
-            let data = ipfs.cat(cid.link, Option::<&str>::None).await?;
-
-            Base::Base64.encode(data)
-        }
-    };
-
-    data_url.push_str(&data);
-
-    Ok(data_url)
 }
 
 /// Retrun a path from date time
--- linked-data/src/media/mime_type.rs
+++ linked-data/src/media/mime_type.rs
@@ -1,31 +1,10 @@
 use crate::types::IPLDLink;
 
-use std::borrow::Cow;
-
 use serde::{Deserialize, Serialize};
-
-use either::Either;
 
 #[derive(Serialize, Deserialize)]
 pub struct MimeTyped {
     pub mime_type: String,
 
-    #[serde(with = "either::serde_untagged")]
-    pub data: Either<IPLDLink, Vec<u8>>,
-}
-
-impl MimeTyped {
-    pub fn new_linked_data(mime_type: impl Into<Cow<'static, str>>, link: IPLDLink) -> Self {
-        Self {
-            mime_type: mime_type.into().into_owned(),
-            data: Either::Left(link),
-        }
-    }
-
-    pub fn new_raw_data(mime_type: impl Into<Cow<'static, str>>, data: Vec<u8>) -> Self {
-        Self {
-            mime_type: mime_type.into().into_owned(),
-            data: Either::Right(data),
-        }
-    }
+    pub data: IPLDLink,
 }
